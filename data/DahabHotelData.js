26c1e2202a5528c08fddf03310feb88d1947d388
fatmaahmed19 <fatomahmed27@gmail.com>
2024-05-04 21:35:46 +0300

    .

---
+++ data/DahabHotelData.js
@@ -0,0 +1,106 @@
+export const DahabHotelData = [
+    {
+      id: 110,
+      name: "Coral Reefs Hotel",
+      destination: "Dahab",
+      price:"EGP 2330 ",
+      description: "Coral Reefs Hotel Dahab features a fitness centre, garden, a shared lounge and bar in Dahab. The accommodation provides a 24-hour front desk, airport transfers, a shared kitchen and free WiFi.",
+      details: {
+        additionalDescription: "Dahab Beach is less than 1 km from the accommodation. The nearest airport is Sharm el-Sheikh International Airport, 93 km from Coral Reefs Hotel Dahab.",
+        photos: ["https://cf.bstatic.com/xdata/images/hotel/max1280x900/342847895.jpg?k=74f98a5f3a4d6307c7b6942957939d34bc76b36a1b9ebbc251335df8621924a4&o=&hp=1"],
+        facilities: ["Free Wi-Fi", "24-hour reception", "Restaurant", "Gym"]
+    },
+    photoURL: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/342846980.jpg?k=40e1a5222949ff1e319a902259e4a4d6dd92ff5e9cef383320097c53a5451e84&o=&hp=1"
+  },
+  {
+    id: 120,
+    name: "Penguin Village Dahab",
+    destination: "Dahab",
+    price:"EGP 7980 ",
+    description: "All accommodation at Penguin Village Dahab is simply furnished. Most rooms include a minibar and a private bathroom with a shower , You can dine at Penguin Restaurant which serves an à la carte menu and provides a 24-hour room service. Diving, snorkelling and windsurfing facilities can be offered upon request, and local excursions can be arranged by the tour desk.",
+    details: {
+      additionalDescription: "Dahab Bus Station is 5 minutes’ drive from the hotel, and Sharm El Sheikh International Airport is 100 km away. Free public parking is possible at a location nearby.",
+      photos: ["https://cf.bstatic.com/xdata/images/hotel/max1280x900/320482666.jpg?k=19eb250ce624f2e77a6e478e7dc73b171a4cc70818bebce800977d9ff932a1c1&o=&hp=1"],
+      facilities: ["Free Wi-Fi", "24-hour reception", "Restaurant", "Gym"]
+  },
+  photoURL: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/450864496.jpg?k=c94b19267b6984d6928d3c0cfd36503c471b2c9fa5d3752615db3dfdca7e8cdf&o=&hp=1"
+},
+{
+    id: 130,
+    name: "Acacia Dahab Hotel",
+    destination: "Dahab",
+    price:"EGP 15650 ",
+    description: "Featuring flat-screen TVs, the air-conditioned rooms at the Acacia Dahab Hotel are fitted with tiled floors and wooden furniture. Free Wi-Fi is available in the hotel’s public areas ,Sahara Restaurant and Grill serves Middle-Eastern and Mediterranean fusion cuisine. Guests can enjoy an informal drink at the Mazaj Cocktail Lounge, featuring live music and Oriental jazz as well as a free billiards table.",
+    details: {
+      additionalDescription: " The Acacia Dahab is located 1 hour’s drive from Sharm el Sheikh International Airport. Airport shuttle service can be arranged by the hotel’s 24-hour front desk.",
+      photos: ["https://cf.bstatic.com/xdata/images/hotel/max1280x900/13902713.jpg?k=999cd392b93b80ba95013b4db23efb28d4f605dbc68931201fc673785208cb11&o=&hp=1"],
+      facilities: ["Free Wi-Fi", "24-hour reception", "Restaurant"]
+  },
+  photoURL: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/413019863.jpg?k=01f4af9c3a4ced35025efb281bffc81e37c791a338457f53add63df79191807a&o=&hp=1"
+},
+{
+    id: 140,
+    name: "Jaz Dahabeya",
+    destination: "Dahab",
+    price:"EGP 5550 ",
+    description: "For those who like to keep fit, Jaz Dahabeya features a well-equipped gym. Water-based activities available include windsurfing, diving and snorkeling at an extra charge. Feel light and beautiful with the spa, which includes reviving and reinvigorating treatments, massage, relaxation programs, sauna and beauty services at an extra charge. Guest can relax, while children are playing at Dahabeya children’s playground.",
+    details: {
+      additionalDescription: " The centre of Dahab is 3 km away. Abu Gallom National Park is a 10-minute drive away. Guests can benefit from the free private parking offered at the hotel.",
+      photos: ["https://cf.bstatic.com/xdata/images/hotel/max1280x900/144507596.jpg?k=a4b5bfb6e71955afc666ab6b8accc86ac272c7252d9c46746fbdae75f595216f&o=&hp=1"],
+      facilities: ["Free Wi-Fi", "24-hour reception", "Restaurant"]
+  },
+  photoURL: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/322912035.jpg?k=b089f9248a528392b7309b009d7ea497d62716a38a1ed75b5bca6bb761212df2&o=&hp=1"
+},
+{
+    id: 150,
+    name: "Swiss Inn Resort Dahab",
+    destination: "Dahab",
+    price:"EGP 18790 ",
+    description: "The bright and airy rooms and suites at the Swiss Inn Resort Dahab have modern amenities, such as a private balcony and a spacious seating area with satellite TV. Some rooms have views of the sea or landscaped gardens , The El Khaima Restaurant and Terrace serves a rich buffet throughout the day. There is also Locanda Sports and Bar. Moreover, Cocktails and mocktails prepared with fresh fruit are served at the Beach Bar.",
+    details: {
+      additionalDescription: "Swiss Inn Resort provides a shuttle service into Dahab's town centre and airport transfers to Sharm El Sheikh International Airport can also be arranged. The resort also offers unique safaris across the Sinai by camel, horse, Jeep or quad bike.",
+      photos: ["https://cf.bstatic.com/xdata/images/hotel/max1280x900/74130024.jpg?k=75a743f04af389f979d6cf9bf3ebe34d0d131f727b444e0a31e2a1d4e74e6c20&o=&hp=1"],
+      facilities: ["Free Wi-Fi", "24-hour reception", "Restaurant","Gym"]
+  },
+  photoURL: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/207872983.jpg?k=32821ebc20c11912ed0c544a88cd29af60ddffeb39e9a53d6b754aeb0b089cde&o=&hp=1"
+},
+{
+    id: 160,
+    name: "Retac Qunay Dahab Resort & SPA",
+    destination: "Dahab",
+    price:"EGP 9220 ",
+    description: "Take your journey to the next level with our exclusive VIP services at Taba Eilat border & Taba Aqaba Boarder. Whether you're arriving or departing, we are here to provide you with unparalleled comfort and convenience. Leave the hassle of border crossing behind and enjoy a stress-free experience with our expert assistance with additional fees.",
+    details: {
+      additionalDescription: " Free Wi-Fi is available in the lobby. The Retac Qunay Dahab Resort is just a 45-minute drive from Sharm El Shiekh.",
+      photos: ["https://cf.bstatic.com/xdata/images/hotel/max1280x900/528367284.jpg?k=20bbad379a4a50baa233c9e3260e087c14f176e5c4b0c3e1b40a27654673b8c5&o=&hp=1"],
+      facilities: ["Free Wi-Fi", "24-hour reception", "Restaurant"]
+  },
+  photoURL: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/500749458.jpg?k=1c2d3991a6aaa76b3cb0da1539a4dcf22df82d6ab3470b33ebfe3e69f3140d71&o=&hp=1"
+},
+{
+    id: 170,
+    name: "Shams Hotel & Dive Centre",
+    destination: "Dahab",
+    price:"EGP 5670 ",
+    description: "Shams Hotel offers a superb beach location in Dahab’s central Mashraba District. It includes Shams restaurant which overlooks the Red Sea. Free Wi-Fi is available in the coffee shop.Air-conditioned rooms and bungalows are simply decorated with private bathroom. Guests can enjoy Red Sea or garden views from their own balcony or terrace. Some have a satellite TV and minibar.",
+    details: {
+      additionalDescription: "The Blue Hole dive location is a 15-minute drive. Sharm el Sheikh International Airport is 90 km away by car.",
+      photos: ["https://cf.bstatic.com/xdata/images/hotel/max1280x900/10794656.jpg?k=ea59129694d9ab3a4d1dd0704f8c8f01fdc8442b37ac65764410908db450faff&o=&hp=1"],
+      facilities: ["Free Wi-Fi", "24-hour reception", "Restaurant"]
+  },
+  photoURL: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/81807367.jpg?k=152958a7aa9e2f9480fe0cf69f476f0771b340ea4a3f7b624754a66124051680&o=&hp=1"
+},
+{
+    id: 180,
+    name: "Villa Boheme & Atelier Boheme",
+    destination: "Dahab",
+    price:"EGP 5863 ",
+    description: "The air-conditioned accommodation includes a seating area with a sofa bed. Each has a kitchen with an oven and a separate dining area. The beds are surrounded by a mosquito net, and the bathroom is decorated with traditional mosaic tiles.",
+    details: {
+      additionalDescription: "Several restaurants and shops are within walking distance of Villa Boheme & Atelier Boheme. The Blue Desert and the Blue Hole, a well-known diving spot, are also nearby.",
+      photos: ["https://cf.bstatic.com/xdata/images/hotel/max1280x900/43997336.jpg?k=d09b733ac07a7dfc731adb5c8275804604b14a7829cc9a50ad725ddd7b4b1763&o=&hp=1"],
+      facilities: ["Free Wi-Fi", "24-hour reception", "Restaurant"]
+  },
+  photoURL: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/7389520.jpg?k=6c43a720b2a411f0d94eb03f8bd093f9a0b44c9a32de28a6d8c269171b6a51d3&o=&hp=1"
+},
+    ]
