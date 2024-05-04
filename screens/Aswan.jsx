6728b34ef17dc7d1ff13902c987d3d6f84b1a047
Nada Mahmoud <na7881709@gmail.com>
2024-05-04 04:21:11 +0300

    Aswan page

---
+++ screens/Aswan.jsx
@@ -0,0 +1,16 @@
+import { View, TextInput, Button, Text, Pressable, StyleSheet, ImageBackground } from "react-native";
+import React, { useState } from "react";
+
+const  Aswan = () => {
+  return (
+    <View>
+        <Text style = {styles.text}>Hello Aswan</Text>
+    </View>
+
+  )};
+  export default Aswan;
+  const styles = StyleSheet.create({
+    text:{
+        fontSize:40,
+    }
+  })
