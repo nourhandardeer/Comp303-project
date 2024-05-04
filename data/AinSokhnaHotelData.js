19cf406d8d0b0ce24aa1c4fbc514cea24b80cb5d
Nada Mahmoud <na7881709@gmail.com>
2024-05-04 21:50:42 +0300

    Create AinSokhnaHotelData.js
    add ainSokhna hotels

---
+++ data/AinSokhnaHotelData.js
@@ -0,0 +1,133 @@
+export const AinSokhnahotelsData = [
+    {
+      id: 1,
+      name: "Dome Marina Hotel & Resort Ain Sokhna",
+      destination: "AinSokhna",
+      price:"EGP 2,877 ",
+      description: "You're eligible for a Genius discount at Dome Marina Hotel & Resort Ain Sokhna! To save at this property,With landscaped gardens overlooking the Gulf of Suez, the Dome Marina Hotel & Resort Ain Sokhna features a freeform outdoor pool and 300 metres of private beach",
+      details: {
+        additionalDescription: "The Ain Soukhna hot springs are located just 20 km from the Dome Marina Hotel, while Cairo is only one hours drive away. The hotel offers free private parking on site.",
+        photos: ["https://cf.bstatic.com/xdata/images/hotel/max1024x768/65549350.jpg?k=314be680c699cf2f88477485983a93c1044ed669ab8e11f613d1de81839e8d6b&o=&hp=1","https://cf.bstatic.com/xdata/images/hotel/max1024x768/142580034.jpg?k=f5da8da62bbd7b7c568240d685a1d9e6bb90d9a8be9e280bdf78763a8d0a3a5a&o=&hp=1","https://cf.bstatic.com/xdata/images/hotel/max1024x768/234599136.jpg?k=9d0bc0eebd81362a43fa857e0915fca84233cc5626492a9cf410b18e37f1033f&o=&hp=1"],
+        facilities: ["Free Wi-Fi", "24-hour reception", "Restaurant"]
+    },
+    photoURL: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/234599102.jpg?k=eb5fc1364c1ca95597bc8eac9fadd9aed25e9a64ceace9966b20123bbbd224a0&o=&hp=1"
+  },
+
+  {
+    id: 2,
+    name: "Jaz Little Venice Golf Resort",
+    destination: "AinSokhna",
+    price:"EGP 7,163 ",
+    description: "With direct access to Ain El Sokhna's beach, this 5-star resort offers modern accommodation with breakfast included.",
+    details: {
+      additionalDescription: "uest can enjoy outstanding 18-hole championship golf course at Sokhna Golf Club with an added change. The tropical pool area offers a relaxing base for sunbathing and swimming.",
+      photos: ["https://cf.bstatic.com/xdata/images/hotel/max1024x768/351730880.jpg?k=e56880981c14fbf0d12b876e5354f3899af2313295aed47fb87db716c719c9b4&o=&hp=1","https://cf.bstatic.com/xdata/images/hotel/max1024x768/321496983.jpg?k=e9b1235b934743fb6dc176bb5308fd4290b0b99495362e1541318bc5f9babc1f&o=&hp=1","https://cf.bstatic.com/xdata/images/hotel/max1024x768/321496168.jpg?k=c858c6ced3de4c6b87fd2dfbf0dd1866d0277d79abd3f93185caadbeaf21d68e&o=&hp=1"],
+      facilities: ["Free Wi-Fi", "24-hour reception", "Restaurant", "Gym"]
+  },
+  photoURL: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/83899173.jpg?k=df7ac97c190e7553e5a933df7a4eaf219a2ea3908d5a2dd583e2c3a1772bd400&o=&hp=1"
+},
+{
+    id: 3,
+    name: "LASIRENA MANI EGYPT-Family Only",
+    destination: "AinSokhna",
+    price:"EGP 3,357 ",
+    description: "Featuring pool views, LASIRENA MANI EGYPT-Family Only in Ain Sokhna provides accommodation, a private beach area, a garden and a shared lounge. This beachfront property offers access to a terrace and free private parking. The rooftop pool features a pool bar and fence.",
+    details: {
+      additionalDescription: "For guests with children, the aparthotel offers an indoor play area, outdoor play equipment and a baby safety gate. Guests can enjoy the indoor pool at LASIRENA MANI EGYPT-Family Only.",
+      photos: ["https://cf.bstatic.com/xdata/images/hotel/max1024x768/546861311.jpg?k=a22d24ce8809dd29ccb8b77031f9c2d4e27031961e8baa1e8e76193437276bc2&o=&hp=1","https://cf.bstatic.com/xdata/images/hotel/max1024x768/546861262.jpg?k=d93997bdb6d92ff1dd4bcd1fa9a3da483f43416b36ef77d0a5af8c229383153a&o=&hp=1","https://cf.bstatic.com/xdata/images/hotel/max1024x768/546861238.jpg?k=4cf7af704b55d1fee2905fcd0ba6ec7161feb90d6c92323a53df772b903e4fb1&o=&hp=1"],
+      facilities: ["Free Wi-Fi", "24-hour reception", "Restaurant", "Gym"]
+  },
+  photoURL: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/546861214.jpg?k=71e73cac4b9d1c0bb10ab935fdceacd4e2da67bb5ee627bc0873cbb360f78f44&o=&hp=1"
+},
+{
+    id: 4,
+    name: "Mövenpick Resort El Sokhna",
+    destination: "AinSokhna",
+    price:"EGP 7,684",
+    description: "Featuring a large outdoor pool and sun-lounger terrace, this 5-star resort offers air-conditioned rooms overlooking the private beach and the Gulf of Suez. It offers a well-equipped fitness centre with a sauna.",
+    details: {
+      additionalDescription: "Panoramic views of the sea and Mediterranean-style cuisine can be enjoyed at El Gezirah Restaurant.  El Sokhna features a fine dining restaurant, and all-day dining in Cataplan Restaurant.",
+      photos: ["https://cf.bstatic.com/xdata/images/hotel/max1024x768/306268403.jpg?k=178c0410e016422b9629edcc51adc25cb47cc1c28d8329d327006cdec772c79b&o=&hp=1","https://cf.bstatic.com/xdata/images/hotel/max1024x768/38593506.jpg?k=0fa2603991094f0b19feb4faf5a5ac19b5f11b0cba144b4505bd007e4b6514e6&o=&hp=1","https://cf.bstatic.com/xdata/images/hotel/max1024x768/38593359.jpg?k=88d39c84e652b7633093a6bacf74b90b44e78f82c13b1c3a243e54f03924b0e6&o=&hp=1"],
+      facilities: ["Free Wi-Fi", "24-hour reception", "Restaurant", "Gym"]
+  },
+  photoURL: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/133932238.jpg?k=59c56395e4e3029fabeb2d2f93d0a4cdbe7fdae7a724d8677da34e3c2ec9ec9a&o=&hp=1"
+},
+{
+    id: 5,
+    name: "Tolip Resort El Galala Majestic",
+    destination: "AinSokhna",
+    price:"EGP 7,960 ",
+    description: "Located in Ain El-Sokhna, Tolip Resort El Galala Majestic offers 5-star resort overlooking the Red Sea and El-Galala Plateau. Swimming pools with bars, restaurants, a lobby lounge, WiFi access, a wellness center and spa are available at the property.",
+    details: {
+      additionalDescription: "The resort features a seasonal heated swimming pool, a well-equipped business area with meeting facilities, a five-a-side court, a 24-hour room service, a front desk and club car services.",
+      photos: ["https://cf.bstatic.com/xdata/images/hotel/max1024x768/263793736.jpg?k=b0e0110f8705ad0c40afb6e18d1ef674b5ad1a00311814fcbb7b6e7d6b88898e&o=&hp=1","https://cf.bstatic.com/xdata/images/hotel/max1024x768/263794557.jpg?k=b92731d5e4d6543eb5cc13f8bf659f19d165426f8dee281cede377e28b1b1c9f&o=&hp=1","https://cf.bstatic.com/xdata/images/hotel/max1024x768/263794714.jpg?k=0e84eeabd0074b4a1a1bcb34044ac7ca7c1d4441e8d6dbd0cd2afa9cee345690&o=&hp=1"],
+      facilities: ["Free Wi-Fi", "24-hour reception", "Restaurant", "Gym"]
+  },
+  photoURL: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/460270312.jpg?k=3448ecbee5c092d38a565ebac2e90cca0095fb6168c9a8ee45f1d6b80a2c52af&o=&hp=1"
+},
+{
+    id: 6,
+    name: "Blumar El Dome Hotel",
+    destination: "AinSokhna",
+    price:"EGP 4,280 ",
+    description: "Blumar El Dome Hotel has a restaurant, seasonal outdoor swimming pool, a garden and terrace in Ain Sokhna. 1.2 km from Porto South Beach and 1.9 km from Porto Sokhna Beach, the property features a private beach area, as well as a bar.",
+    details: {
+      additionalDescription: "The hotel will provide guests with air-conditioned rooms offering a wardrobe, a kettle, a minibar, a safety deposit box, a flat-screen TV, a balcony and a private bathroom with a bath or shower. At Blumar El Dome Hotel rooms come with bed linen and towels.",
+      photos: ["https://cf.bstatic.com/xdata/images/hotel/max1024x768/383102663.jpg?k=e18bd29d7af11904dbfdc4af359d8ac74d02f86edda3aa4f70c2cbdef124d26f&o=&hp=1","https://cf.bstatic.com/xdata/images/hotel/max1024x768/388602988.jpg?k=71fbabae2b7cb3a05f07ef33c75abdaba7d04ca70724153b6454d5ba5a8973ed&o=&hp=1","https://cf.bstatic.com/xdata/images/hotel/max1024x768/388603544.jpg?k=9960ed8da58912ec809ca6c16503aa0a57801a37baac7d1c27c52f280fb9f0b1&o=&hp=1"],
+      facilities: ["Free Wi-Fi", "24-hour reception", "Restaurant", "Gym"]
+  },
+  photoURL: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/388884330.jpg?k=4e4eb55ba9de53eb77282a9fe4c1ac012c23d95cd3fecee1ee4bab369f454a29&o=&hp=1"
+},
+{
+    id: 7,
+    name: "Coral Sea Beach and Aqua Park",
+    destination: "AinSokhna",
+    price:"EGP 7,480 ",
+    description: "You're eligible for a Genius discount at Coral Sea Beach and Aqua Park! To save at this property",
+    details: {
+      additionalDescription: "Set on a private beach and offering fantastic views of El Sokhna, the Coral Sea Beach is located in Ain El Sokhna. It features comfortable, air-conditioned rooms, a heated indoor swimming pool, a large outdoor pool and an Aqua Park .",
+      photos: ["https://cf.bstatic.com/xdata/images/hotel/max1024x768/427370214.jpg?k=cb8909eac701462a594cebd821a19497e1a137a6aa7ba3f5d40c9a73697d6a1e&o=&hp=1","https://cf.bstatic.com/xdata/images/hotel/max1024x768/420925425.jpg?k=1c5e33cb47b15383fd763225b6092e71ebe5f6f571a31de131c952884d4aacc7&o=&hp=1","https://cf.bstatic.com/xdata/images/hotel/max1024x768/420923104.jpg?k=17140566357eca3fc2ded3e8f8df2c1ed7fcf5bdf344a423a4d47fc351f38752&o=&hp=1"],
+      facilities: ["Free Wi-Fi", "24-hour reception", "Restaurant", "Gym"]
+  },
+  photoURL: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/420923207.jpg?k=7c66112fab9902875fd50a8e47f6c143f3d567fdd384cf88c40e36488484c4e7&o=&hp=1"
+},
+{
+    id: 8,
+    name: "Porto Sokhna Beach Resort",
+    destination: "AinSokhna",
+    price:"EGP  4,028 ",
+    description: "Porto Sokhna is a 4-star hotel offering spacious, self-catering and serviced accommodations, personalized service, and resort-style facilities in Ain Sokhna.",
+    details: {
+      additionalDescription: "Porto Sokhna Beach Resort, on-site restaurant offers a rich breakfast buffet and traditional regional meals are prepared for dinner. Free parking is offered to all guests.",
+      photos: ["https://cf.bstatic.com/xdata/images/hotel/max1024x768/302110889.jpg?k=a02e37bb623aadddb98a873b1d1a2d3dbf54527cec213766f3d24622a7420092&o=&hp=1","https://cf.bstatic.com/xdata/images/hotel/max1024x768/302110827.jpg?k=eb1d2dc29688451e0796ad2d25c5b3b9bf7e9d705d79c01ad441dc250385c387&o=&hp=1","https://cf.bstatic.com/xdata/images/hotel/max1024x768/49217368.jpg?k=b3eecc51b29b174b3818b876984bac13a3719a8de4131eb8a8c57530a8fd34a1&o=&hp=1"],
+      facilities: ["Free Wi-Fi", "24-hour reception", "Restaurant", "Gym"]
+  },
+  photoURL: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/95666653.jpg?k=dcb06630afcf88b5ffb23b2e531e1415e8585110e0c405fa640a0d5a783a23ae&o=&hp=1"
+},
+{
+    id: 9,
+    name: "Kefi Palmera Beach Resort",
+    destination: "AinSokhna",
+    price:"EGP 4,919 ",
+    description: "The 3-Star ZYA Palmera Beach Resort offers a private Red Sea beachfront location. The 76 rooms are modern and spacious with furnished seating area and private balcony or terrace. There are 4 restaurants, 3 bars and 3 pools.",
+    details: {
+      additionalDescription: "Guests can enjoy á la carte local and international cuisine or there is a buffet breakfast and dinner available. Palmera has a beach bar, jazz bar and a late night billiard bar.",
+      photos: ["https://cf.bstatic.com/xdata/images/hotel/max1024x768/95687678.jpg?k=ac90361d40b501214179441f6ad09a62a5bc00c1bc5edff4a523198e6a37d290&o=&hp=1","https://cf.bstatic.com/xdata/images/hotel/max1024x768/95919544.jpg?k=d4f9d0e0cb3c5d1577d04118a2432a57bff1441eef9b175911426076312bd14f&o=&hp=1","https://cf.bstatic.com/xdata/images/hotel/max1024x768/95676839.jpg?k=06cb0c4c9ae1b4105614b24af9d5f391398dc436b81772c247131b6f77a4feb9&o=&hp=1"],
+      facilities: ["Free Wi-Fi", "24-hour reception", "Restaurant", "Gym"]
+  },
+  photoURL: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/95687661.jpg?k=05a55f37497a21982d0e49b189b5acb03f8b2721b9a646b703e4a55690e65b17&o=&hp=1"
+},
+{
+    id: 10,
+    name: "Elite Residence & Aqua Park",
+    destination: "AinSokhna",
+    price:"EGP 10,309 ",
+    description: "Situated in Ain Sokhna, 34 km from Suez Canal, Elite Residence & Aqua Park boasts a restaurant. The hotel has an outdoor pool, and indoor heated swimming pool and a private beach area, and guests can enjoy a meal at the restaurant or a drink at the bar. Free private parking is available on site.",
+    details: {
+      additionalDescription: "Guests can enjoy an energizing workout at the gym or relaxing massage treatments, Sauna, Steam Jacuzzi or Beauty Sessions at the Green Spa.",
+      photos: ["https://cf.bstatic.com/xdata/images/hotel/max1024x768/69188301.jpg?k=3f5105127f408a0b8ff171db978d53ca09ac923904ed0f02a45c8e3629cf921a&o=&hp=1","https://cf.bstatic.com/xdata/images/hotel/max1024x768/69188300.jpg?k=64b0aabc9ff6826aef77ce4112e50e94e2a7807254dc384877f6b2ceab4e3725&o=&hp=1","https://cf.bstatic.com/xdata/images/hotel/max1024x768/114021707.jpg?k=a7e7aa5a1f5f85bc816ea27489b206c077c8d7b7c0a0d3898c185a4535dd1acf&o=&hp=1"],
+      facilities: ["Free Wi-Fi", "24-hour reception", "Restaurant", "Gym"]
+  },
+  photoURL: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/437621356.jpg?k=a465ec8aef90dc548bfbf33e03206031b835af0d1f366a8c66abc4000e2053ed&o=&hp=1"
+},
+    ]
