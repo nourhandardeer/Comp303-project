1ea352844e865a2c1b3148a628bb2c0a369df318
Nada Mahmoud <na7881709@gmail.com>
2024-05-04 22:30:01 +0300

    add profile screen

---
+++ screens/Profile.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, FlatList, Image,TouchableOpacity } from 'react-native';
+import { collection, addDoc, getDocs, query, where,doc,setDoc } from 'firebase/firestore';
+import { db } from '../firebase/config';
+import { FontAwesome } from '@expo/vector-icons';
+import { useNavigation, useRouter, useLocalSearchParams ,router} from "expo-router";
+import { AinSokhnahotelsData} from '../data/AinSokhnaHotelData';
+import Header from '../components/header'
+
+export default function Profile(){
+
+
+return(
+
+
+    <View>
+        <Text>
+            hello
+        </Text>
+    </View>
+)
+
+
+
+};
