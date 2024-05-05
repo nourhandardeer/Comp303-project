82210d882b21a3bc4e890f143dbc196c22762150
Nada Mahmoud <na7881709@gmail.com>
2024-05-06 01:18:38 +0300

    create a calendar page

---
+++ app/Calendar.js
@@ -0,0 +1,6 @@
+import  CalendarInput from '../screens/calendar'
+export default function Page() {
+    return (
+     <CalendarInput/>
+    );
+  }
--- screens/home.js
+++ screens/home.js
@@ -34,6 +34,9 @@ export default function App() {
         {/* <Pressable style={styles.button} onPress={() => router.replace("/Profile")} >
             <Text style={styles.buttonText}>Profile</Text>
           </Pressable> */}
+          <Pressable style={styles.button} onPress={() => router.replace("/Calendar")} >
+            <Text style={styles.buttonText}>calendar</Text>
+          </Pressable>
         <Text style={styles.qouteText}> Prepare to be enchanted by the wonders of this timeless land,</Text>
         <Text style={styles.qouteText}> where every corner holds a story waiting to be discovered</Text>
         
