ef5f764a0ca97f357a9a255b59ac3c086437e9e0
Nada Mahmoud <na7881709@gmail.com>
2024-05-10 03:25:06 +0300

    fetch  reservations of user

---
+++ screens/Reservation.jsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, FlatList, ImageBackground, Image, TouchableOpacity, Pressable } from 'react-native';
+import { collection, addDoc, getDocs, query, where, doc, setDoc, getDoc } from 'firebase/firestore';
+import { db } from '../firebase/config';
+import { app } from '../firebase/config'
+import { getAuth } from "firebase/auth";
+
+export default function Reservation() {
+    const [error, setError] = useState(null);
+    const [reservervations, setReservations] = useState(null);
+    const auth = getAuth(app);
+    useEffect(() => {
+        handelGetUseReservation();
+      }, []);
+
+      let y=[];
+      const handelGetUseReservation = async () => {
+
+        try {
+        const Ref = collection(db, "Reservations");
+        const q = query(Ref, where("userId", "==", auth.currentUser.uid));
+        
+        const querySnapshot = await getDocs(q);
+       
+        
+        querySnapshot.forEach((doc) => {
+         
+          // doc.data() is never undefined for query doc snapshots
+        
+          y.push({...doc.data()});
+       
+          //console.log(doc.id, " => ", doc.data(),"nadaaaaaaaaaaaa");
+
+        });
+    }catch (error) {
+        console.error('Error fetching user:', error);
+        setError('Failed to fetch user reservations. Please try again later.');
+        // Handle error
+      }
+    //console.log(y)
+    
+    setReservations(y);
+    //console.log(reservervations)
+      };
+      
+
+return(
+<View style={styles.container}>
+    <View style={styles.bookstyle}>
+<Text> Your Reservervations</Text>
+</View>
+
+    
+</View>
+);
+
+
+};
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: 20, // Add padding at the top
+      paddingHorizontal: 20,
+    },
+    darkBackground: {
+      backgroundColor: '#333', // Dark mode background color
+    },
+    listContent: {
+      paddingBottom: 20, // Add padding at the bottom
+    },
+    hotelItem: {
+      marginBottom: 20,
+      borderWidth: 1,
+      borderColor: '#ddd',
+      borderRadius: 10,
+      overflow: 'hidden',
+      position: 'relative',
+    },
+ 
+   
+    list:{
+      marginTop:30,
+    },
+    bookstyle:{
+  backgroundColor:'#D8A123',
+  alignItems:'center',
+   justifyContent:'center',
+   width:'50%',
+   height:35,
+   marginLeft:90,
+   
+    },
+    bookTitle:{
+      
+   marginLeft:150,
+   color:'#D8A123',
+   marginVertical:10
+    }
+  });
